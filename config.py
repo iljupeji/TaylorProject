4d4b963b37f05374af2c9a13fa169c94964e2f25
iljpj74@gmail.com <iljpj74@gmail.com>
2026-02-05 20:13:21 -0600

    Ignorar extractor.py y preparar otros cambios

--- config.py
+++ config.py
@@ -1,10 +1,7 @@
 import os
-#print(f"Directorio actual:{os.getcwd()}")
+print(f"Directorio actual:{os.getcwd()}")
 from dotenv import load_dotenv
-import lyricsgenius
-
 load_dotenv()
 token=os.getenv('GENIUS_ACCESS_TOKEN')
 os.environ['GENIUS_ACCESS_TOKEN']=token
-#print(os.environ)
-genius=lyricsgenius.Genius(token)
+print(os.environ)
